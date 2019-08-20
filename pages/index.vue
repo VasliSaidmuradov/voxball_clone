6ba15eff7f0880ae9c6bb5287c579ad23ae37cd1
Damir <https://gitlab.com/Kakharov_Damir>
2019-08-21 01:12:08 +0600

    Poll card

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,13 @@
         </Carousel>
       </no-ssr>
     </section>
-    <section class="Top-poll"></section>
+    <section class="Top-poll container">
+      <ul class="Top-poll__list">
+        <li class="Top-poll__item" v-for="(poll, index) in 5" :key="index">
+          <poll-card></poll-card>
+        </li>
+      </ul>
+    </section>
     <section class="Advertising"></section>
     <section class="Top-news"></section>
     <section class="Analitic-articles"></section>
@@ -20,20 +26,46 @@
 if (process.browser) {
   var { Carousel, Slide } = require('vue-carousel')
 }
+
+import pollCard from '@/components/cards/pollCard.vue'
 export default {
   components: {
     Carousel,
-    Slide
+    Slide,
+    pollCard
   },
   data() {
     return {
-      page: 0
+      page: 0,
+      pollCards: [
+        {
+          name: 'sdfds fdsf dsfsd f '
+        }
+      ]
     }
   },
   methods: {}
 }
 </script>
 
-<style>
+<style lang="scss">
+.Top-poll {
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    margin: 0 -15px;
+    margin-top: 20px;
+    list-style: none;
+    padding: 0;
+    font-size: 0.8em;
+  }
+  &__item {
+    flex: 0 0 auto;
+    margin: 15px;
+    margin-bottom: 0;
+    width: calc(1 / 5 * 100% - 30px);
+  }
+}
 </style>
 
