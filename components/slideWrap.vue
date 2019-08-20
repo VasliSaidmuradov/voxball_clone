18eebd0fef717801be65e741b848f73c9ebefbf5
Sultan <tochkaapi@mail.ru>
2019-08-20 13:47:31 +0600

    main page slider

---
+++ components/slideWrap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+        <p>Участвуй в опросе</p>
+        <p>
+            или создай свои опросы пройдя регистрацию.
+            И зарабатывай коины. Нам важен твой голос.
+        </p>
+        <nuxt-link>подробнее</nuxt-link>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
