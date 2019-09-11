1719527b8de851b76ea65ad76adf510b2838a58a
Damir <https://gitlab.com/Kakharov_Damir>
2019-09-11 17:45:03 +0600

    added newsList

---
+++ components/news/newsList/newsList.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="container">
+    <div class="row">
+      <div class="col-6 d-flex flex-column justify-content-between">
+        <new-item v-if="news[0]" :data="news[0]" class="Analitic-articles__card"></new-item>
+        <new-item v-if="news[1]" :data="news[1]" class="Analitic-articles__card"></new-item>
+      </div>
+      <div class="col-3">
+        <new-item v-if="news[2]" :data="news[2]" class="Analitic-articles__card" column></new-item>
+      </div>
+      <div class="col-3">
+        <new-item v-if="news[3]" :data="news[3]" class="Analitic-articles__card" column></new-item>
+      </div>
+      <div class="col-3">
+        <new-item v-if="news[4]" :data="news[4]" class="Analitic-articles__card" column></new-item>
+      </div>
+      <div class="col-3">
+        <new-item v-if="news[5]" :data="news[5]" class="Analitic-articles__card" column></new-item>
+      </div>
+      <div class="col-6 d-flex flex-column justify-content-between">
+        <new-item v-if="news[6]" :data="news[6]" class="Analitic-articles__card"></new-item>
+        <new-item v-if="news[7]" :data="news[7]" class="Analitic-articles__card"></new-item>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import newItem from '@/components/news/newslist/newsItem.vue'
+import advertising from '@/components/advertising.vue'
+
+export default {
+  components: {
+    advertising,
+    newItem
+  },
+  props: {
+    news: Array
+  },
+  created() {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/news/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <section class="section">
+      <div class="section__header">
+        <h2 class="section__title">Аналитические статьи</h2>
+        <p class="section__text">Мнения и факты авторитетных журналистов и популярных блогеров</p>
+      </div>
+    </section>
+
+    <news-list v-for="(neww, index) in news" :key="index" :news="neww"></news-list>
+  </div>
+</template>
+
+<script>
+import newsList from '@/components/news/newsList/newsList.vue'
+
+export default {
+  components: {
+    newsList
+  },
+  data() {
+    return {
+      news: [
+        [
+          {
+            name: 'dsfsdfdsf'
+          },
+          {
+            name: 'dsfsdfdsfsdf'
+          },
+          {
+            name: 'dsfsdfdsfsdf'
+          },
+          {
+            name: 'dsfsdfdsfsdf'
+          },
+          {
+            name: 'dsfsdfdsfsdf'
+          }
+        ],
+        [
+          {
+            name: 'dsfsdfdsf'
+          },
+          {
+            name: 'dsfsdfdsfsdf'
+          },
+          {
+            name: 'dsfsdfdsfsdf'
+          }
+        ]
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
