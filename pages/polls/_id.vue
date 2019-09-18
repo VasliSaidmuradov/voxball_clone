1d988bf3f06271832c5336552c3df4038a040beb
Damir <https://gitlab.com/Kakharov_Damir>
2019-09-18 15:16:12 +0600

    add commet component

--- pages/polls/_id.vue
+++ pages/polls/_id.vue
@@ -3,6 +3,8 @@
     <detailed-layout :title="'Мои Опросы'">
       <poll-info class="mt-5" :poll="pollData"></poll-info>
       <poll-card class="mb-5" :answers="answers"></poll-card>
+
+      <comment-list :dataList="comments" />
     </detailed-layout>
   </div>
 </template>
@@ -11,12 +13,14 @@
 import detailedLayout from '@/components/layouts/detailedLayout.vue'
 import pollInfo from '@/components/polls/poll/pollInfo.vue'
 import pollCard from '@/components/polls/poll/pollCard.vue'
+import commentList from '@/components/comments/commetsList/commentList.vue'
 
 export default {
   components: {
     detailedLayout,
     pollInfo,
-    pollCard
+    pollCard,
+    commentList
   },
   data() {
     return {
@@ -35,6 +39,43 @@ export default {
         { label: 'No' },
         { label: 'Maybe' },
         { label: 'Yes, No, Maybe' }
+      ],
+      comments: [
+        {
+          author: 'vova',
+          createdAt: new Date().toLocaleDateString(),
+          id: 1,
+          parent: undefined,
+          child: undefined,
+          content: 'lorem dewde wed ewd ew d  ew d  ew d  ew d we   dw'
+        },
+        {
+          author: 'vova1',
+          createdAt: new Date().toLocaleDateString(),
+          id: 2,
+          parent: undefined,
+          content: 'lorem dewde wed ewd ew d  ew d  ew d  ew d we   dw',
+
+          child: [
+            {
+              author: 'vovach',
+              createdAt: new Date().toLocaleDateString(),
+              id: 4,
+              parent: 2,
+              child: undefined,
+              content: 'lorem dewde wed ewd ew d  ew d  ew d  ew d we   dw'
+            },
+
+            {
+              author: 'vovach5',
+              createdAt: new Date().toLocaleDateString(),
+              id: 5,
+              parent: 2,
+              child: undefined,
+              content: 'lorem dewde wed ewd ew d  ew d  ew d  ew d we   dw'
+            }
+          ]
+        }
       ]
     }
   }
